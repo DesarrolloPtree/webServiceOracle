132131497db6f0315c2ccb2d56e0af9e465bd77e
SergioMayaR <saxel.12194@gmail.com>
2021-03-16 23:16:14 -0600

    l

--- Routes/empleoyes.js
+++ Routes/empleoyes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const router = express.Router()
+const router = express.Router();
 const oracledb = require('oracledb');
 
 router.get("/api/empleoyes", (req, res) => {
